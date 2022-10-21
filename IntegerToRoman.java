7a138489c01fcefb37858cbf1b7aeeca19940a8b
Prafful-Shukla <90524387+Prafful-Shukla@users.noreply.github.com>
2022-10-21 17:45:05 +0530

    Create IntegerToRoman.java

---
+++ IntegerToRoman.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String intToRoman(int num) {
+        StringBuilder res = new StringBuilder();
+        
+        int arr[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String symbol[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        
+        for(int i=0;i<=arr.length-1&&num>0;i++){
+            while(num>=arr[i]){
+                num=num-arr[i];
+                res.append(symbol[i]);
+            }
+        }
+        return res.toString();
+    }
+}
